3d4cdf1451f7c6371155826312acfd2985d929f8
HP <poposki.p00@gmail.com>
2023-02-12 16:49:48 +0100

    Added Recycler View for User Changed Display of Recycler layout for admin Added new Recycler layout for user

--- app/src/main/java/com/example/taxiapp/UserActivity.java
+++ app/src/main/java/com/example/taxiapp/UserActivity.java
@@ -4,6 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,21 +14,42 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.internal.NavigationMenuItemView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity {
+    private DatabaseReference lastRef;
+    private StorageReference storageReference;
     private TextView userEmail;
     private FirebaseAuth mAuth;
     private NavigationMenuItemView LogoutNav;
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    RecyclerView mRecyclerView;
+    myAdapter mAdapter;
+    List<Driver> values;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        mAuth = FirebaseAuth.getInstance();
+        values = new ArrayList<Driver>();
+        storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://taxiapp-70702.appspot.com/drivers/Petar/image.jpg");
+        storageReference = FirebaseStorage.getInstance("gs://taxiapp-70702.appspot.com").getReference();
+        lastRef = FirebaseDatabase.getInstance("https://taxiapp-70702-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
         mAuth = FirebaseAuth.getInstance();
 
         // drawer layout instance to toggle the menu icon to open
@@ -38,15 +62,52 @@ public class UserActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        // to make the Navigation drawer icon always appear on the action bar
-        getSupportActionBar().setDisplayShowHomeEnabled(false);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(false);
+
 
         Intent intent = getIntent();
         String email = intent.getStringExtra("message");
         userEmail = findViewById(R.id.user_email);
         userEmail.setText("The user's email is: " + email);
+
+        //сетирање на RecyclerView контејнерот
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+// оваа карактеристика може да се користи ако се знае дека промените
+// во содржината нема да ја сменат layout големината на RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+// ќе користиме LinearLayoutManager
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+// и default animator (без анимации)
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+// сетирање на кориснички дефиниран адаптер myAdapter (посебна класа)
+        mAdapter = new myAdapter(values, R.layout.userview_row, this);
+//прикачување на адаптерот на RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+
+        lastRef.child("drivers").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot snappy: snapshot.getChildren())
+                {
+                    Driver driver = snappy.getValue(Driver.class);
+                    //StorageReference stor = storageReference.child("drivers").child(driver.getName()).child("image.jpg");
+                    String url = "https://firebasestorage.googleapis.com/v0/b/taxiapp-70702.appspot.com/o/drivers%2F" + driver.getName() +"%2Fimage?alt=media";
+                    driver.setImage(url);
+                    values.add(driver);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserActivity.this, "HELLO" + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(false);
+
     }
 
     @Override
